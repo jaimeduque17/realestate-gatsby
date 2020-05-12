d114655976155f7cd36a4aacc5beb58b076c4351
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-12 18:57:46 -0500

    header styles are added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { css } from '@emotion/core'
 import Navigation from './navigation'
 
 const Header = () => {
     return (
-        <header>
-            <div>
+        <header css={css`
+            background-color: #0D283B;
+            padding: 1rem;
+        `}>
+            <div css={css`
+                max-width: 120rem;
+                margin: 0 auto;
+                text-align: center;
+
+                @media (min-width: 768px) {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                }
+            `}>
                 <Link>Real Estate</Link>
                 <Navigation />
             </div>
