e50d9edae2fa6247198fd3252297b30191983c9c
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-11 22:56:37 -0500

    global styles are created

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,8 +7,42 @@ const Layout = ({ children }) => {
         <>
             <Global 
                 styles={css`
+                    html {
+                        font-size: 62.5%;
+                        box-sizing: border-box;
+                    }
+                    *, *:before, *:after {
+                        box-sizing: inherit;
+                    }
                     body {
-                        background-color: blue;
+                        font-size: 1.6rem;
+                        line-height: 2;
+                        font-family: 'Lato', sans-serif; 
+                    }
+                    h1, h2, h3 {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        text-align: center;
+                        font-family: 'Lato', sans-serif;
+                        font-weight: 300;
+                    }
+                    h3 {
+                        font-family: 'Roboto', sans-serif;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                    .container {
+                        max-width: 120rem;
+                        margin: 0 auto;
+                        width: 95%;
+                    }
+                    img {
+                        max-width: 100%;
                     }
                 `}
             />
