03eeb1ec7619a2e5e6a505240a3c4967c316a32a
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-12 19:24:38 -0500

    navigation styles are added

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+
+const Nav = styled.nav`
+    display: flex;
+    flex-direction: column;
+    padding: 0 0 3rem 0;
+
+    @media (min-width: 768px) {
+        padding: 0;
+        flex-direction: row;
+    }
+`;
+
+const NavLink = styled(Link)`
+    color: #FFF;
+    font-weight: 700;
+    font-family: 'PT Sans', sans-serif;
+    text-decoration: none;
+    padding: 0.5rem;
+    margin-right: 1rem;
+    &:last-of-type {
+        margin-right: 0;
+    }
+    &.actual-page {
+        border-bottom: 2px solid #FFF;
+    }
+    
+`;
+
+// &:last-of-us --> pseudo selector
 
 const Navigation = () => {
     return (
-        <nav>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/about'}>About</Link>
-            <Link to={'/properties'}>Properties</Link>
-        </nav>
+        <Nav>
+            <NavLink
+                to={'/'}
+                activeClassName="actual-page"
+            >
+                Home
+            </NavLink>
+            <NavLink
+                to={'/about'}
+                activeClassName="actual-page"
+            >
+                About
+            </NavLink>
+            <NavLink
+                to={'/properties'}
+                activeClassName="actual-page"
+            >
+                Properties
+            </NavLink>
+        </Nav>
     )
 }
 
