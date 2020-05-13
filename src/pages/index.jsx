58fcf9dcb1e7136c9cd7a5afb8999542ab54125e
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-12 22:43:20 -0500

    the content is displayed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout'
 import useHome from '../hooks/usehome'
+import { css } from '@emotion/core'
+import styled from '@emotion/styled'
 
 const Index = () => {
 
@@ -10,7 +12,18 @@ const Index = () => {
 
     return (
         <Layout>
+            <main>
+                <div css={css`
+                    max-width: 800px;
+                    margin: 0 auto;
+                `}>
+
             <h1>{name}</h1>
+            <p css={css`
+                text-align: center;
+            `}>{content}</p>
+                </div>
+            </main>
         </Layout>
     )
 }
