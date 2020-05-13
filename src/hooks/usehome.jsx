1e269736f65f54f45bf216ce7166ac91cb95943c
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-12 22:26:06 -0500

    grapgql is used to displayed the information

---
+++ src/hooks/usehome.jsx
@@ -0,0 +1,33 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+const useHome = () => {
+    
+    const result = useStaticQuery(graphql`
+        query {
+            allStrapiPages(filter: {name: {eq: "Home"}}) {
+                nodes {
+                    id
+                    name
+                    content
+                    image {
+                        sharp: childImageSharp {
+                            fluid(maxWidth: 1200) {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    `)
+
+    return result.allStrapiPages.nodes.map(home => ({
+        name: home.name,
+        content: home.content,
+        image: home.image
+    }))
+
+}
+
+export default useHome
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout'
+import useHome from '../hooks/usehome'
 
 const Index = () => {
+
+    const home = useHome()
+
+    const { name, content, image } = home[0]
+
     return (
         <Layout>
-            <h1>Index</h1>
+            <h1>{name}</h1>
         </Layout>
     )
 }
