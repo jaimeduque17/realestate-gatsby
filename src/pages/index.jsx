20529a71bcd191f0cbe0113b478a88a8fd354be0
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-13 22:50:15 -0500

    css file is used to add styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import useHome from '../hooks/usehome'
 import { css } from '@emotion/core'
 import styled from '@emotion/styled'
 import BackgroundImage from 'gatsby-background-image'
+import heroCSS from '../css/hero.module.css'
 
 const ImageBackground = styled(BackgroundImage)`
     height: 600px;
@@ -22,8 +23,8 @@ const Index = () => {
                 fluid={image.sharp.fluid}
                 fadeIn="soft"
             >
-                <div>
-                    <h1>Sale of exclusive houses and apartments</h1>
+                <div className={heroCSS.imagebg}>
+                    <h1 className={heroCSS.title}>Sale of exclusive houses and apartments</h1>
                 </div>
             </ImageBackground>
             <main>
