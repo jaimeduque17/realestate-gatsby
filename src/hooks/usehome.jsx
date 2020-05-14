a511265c3d44a09f7fd926bb9320537ab5708855
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-13 22:40:32 -0500

    transformer sharp is used in the background image

--- src/hooks/usehome.jsx
+++ src/hooks/usehome.jsx
@@ -11,7 +11,9 @@ const useHome = () => {
                     content
                     image {
                         sharp: childImageSharp {
-                            fluid(maxWidth: 1200) {
+                            fluid(maxWidth: 1500 duotone: {
+                                highlight: "#222222", shadow: "#192550", opacity: 30
+                            }) {
                                 ...GatsbyImageSharpFluid_withWebp
                             }
                         }
