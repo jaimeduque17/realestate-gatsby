f2e3e3fe8364d3ad7afeb2ae3d1ee72118aaea5f
Jaime Duque <jaime.duque@estratek.com.co>
2020-05-18 18:28:37 -0500

    dynamic pages are linked

--- src/components/propertyPreview.jsx
+++ src/components/propertyPreview.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import Image from 'gatsby-image'
+import { Link } from 'gatsby'
+import UrlSlug from 'url-slug'
 import Icons from './icons'
+
+const ButtonCard = styled(Link)`
+    margin: 2rem 0 0 0;
+    padding: 1rem;
+    background-color: #75ab00;
+    width: 100%;
+    color: #FFF;
+    display: block;
+    text-decoration: none;
+    text-align: center;
+    font-weight: 700;
+    text-transform: uppercase;
+`
 
 const Card = styled.div`
     border: 1px solid #E1E1E1;
@@ -40,6 +55,9 @@ const PropertPreview = ({ property }) => {
                     parking={parking}
                     rooms={rooms}
                 />
+                <ButtonCard to={UrlSlug(name)}>
+                    more details
+                </ButtonCard>
             </Content>
         </Card>
     )
